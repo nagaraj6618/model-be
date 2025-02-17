a3c16023a33efbfbaca638bc748d78564ad948f1
nagaraj6618 <nagaraj516700@gmail.com>
2025-02-18 01:02:17 +0530

    Changed to md size model

--- main.py
+++ main.py
@@ -11,39 +11,6 @@ def welcome():
     }
     return response
 
-@app.route('/about')
-def about():
-    return render_template('about.html')
-
-@app.route('/service')
-def service():
-    return render_template('service.html')
-
-
-@app.route('/source')
-def source():
-    return render_template('source.html')
-
-@app.route('/contact')
-def contact():
-    return render_template('contact.html')
-
-@app.route('/getAudio')
-def getAudio():    
-    return "Geting Audio.."
-    # r = sr.Recognizer()
-    # duration = 55
-    # with sr.Microphone() as source:
-    #     audio_data = r.listen(source, timeout=duration)
-
-    # try:
-    #     text = r.recognize_google(audio_data)  # Use recognize_google if you want to use the Google API
-    #     return jsonify({"text": text})
-    # except sr.UnknownValueError:
-    #     return jsonify({"text": "Sorry, couldn't understand the audio"})
-    # except sr.RequestError as e:
-    #     return jsonify({"text": f"Error with request to Google Speech Recognition service: {e}"})
-
 @app.route('/calculate_score', methods=['POST'])
 def calculate_score():
     data = request.get_json()  # Expecting JSON input
